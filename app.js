94579d22a096a51087e89e0fea027941aeda3317
sminiduck <sminiduck@gmail.com>
2026-06-05 00:15:39 +0900

    chore: add Prettier and format codebase

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-import {
-  probBook,
-  checkAnsBook
-} from "./data.js";
+import { probBook, checkAnsBook } from './data.js';
 
 const INITIAL_STATE = {
   mode: null,
@@ -11,14 +8,20 @@ const INITIAL_STATE = {
   score: 0,
   countProb: 0,
   total: 0,
-  countWrong: 0
+  countWrong: 0,
 };
 
-const state = { ...INITIAL_STATE , currPage: "menu"};
+const state = { ...INITIAL_STATE, currPage: 'menu' };
 
-const $selectMode = document.getElementById("mode");
+function movePage(moveId) {
+  document.getElementById(state.currPage).style.display = 'none';
+  document.getElementById(moveId).style.display = 'block';
+  state.currPage = moveId;
+}
+
+const $selectMode = document.getElementById('mode');
 for (const el of probBook.Ids()) {
-  const option = document.createElement("option");
+  const option = document.createElement('option');
 
   option.value = el.value;
   option.textContent = el.text;
@@ -26,9 +29,9 @@ for (const el of probBook.Ids()) {
   $selectMode.appendChild(option);
 }
 
-const $selectAccuracy = document.getElementById("accuracy");
+const $selectAccuracy = document.getElementById('accuracy');
 for (const el of checkAnsBook.Ids()) {
-  const option = document.createElement("option");
+  const option = document.createElement('option');
 
   option.value = el.value;
   option.textContent = el.text;
@@ -36,45 +39,35 @@ for (const el of checkAnsBook.Ids()) {
   $selectAccuracy.appendChild(option);
 }
 
-
-const $menuForm = document.getElementById("menuForm");
-
-$menuForm.addEventListener("submit", (e) => {
+const $menuForm = document.getElementById('menuForm');
+$menuForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const data = new FormData($menuForm);
 
-  state.mode = data.get("mode");
+  state.mode = data.get('mode');
   state.probGen = probBook.select(state.mode);
-  state.total = Number(data.get("total"));
-  const accuarcy = data.get("accuracy");
-  const detail = Number(data.get("detail"));
+  state.total = Number(data.get('total'));
+  const accuarcy = data.get('accuracy');
+  const detail = Number(data.get('detail'));
   state.checkAns = checkAnsBook.select(accuarcy, detail);
 
-  movePage("quiz");
+  movePage('quiz');
   nextProblem();
 });
-
-function movePage(moveId) {
-  document.getElementById(state.currPage).style.display = "none";
-  document.getElementById(moveId).style.display = "block";
-  state.currPage = moveId;
-}
 
 function nextProblem() {
   state.currProb = state.probGen();
   state.countProb += 1;
   state.countWrong = 0;
-  document.getElementById("problem").textContent = state.currProb.probDisp;
-  document.getElementById("answer").value = "";
+  document.getElementById('problem').textContent = state.currProb.probDisp;
+  document.getElementById('answer').value = '';
   console.log(state);
 }
 
+const $keypad = document.getElementById('keypad');
 
-
-const $keypad = document.getElementById("keypad");
-
-$keypad.addEventListener("click", (e) => {
+$keypad.addEventListener('click', (e) => {
   const button = e.target;
 
   if (button.dataset.value) {
@@ -83,103 +76,97 @@ $keypad.addEventListener("click", (e) => {
   }
 
   switch (button.dataset.action) {
-    case "allClear":
+    case 'allClear':
       allClear();
       break;
 
-    case "backspace":
+    case 'backspace':
       backspace();
       break;
 
-    case "submit":
+    case 'submit':
       submitAnswer();
       break;
 
-    case "wrongNext":
+    case 'wrongNext':
       wrongNext();
       break;
   }
 });
 
 function allClear() {
-  document.getElementById("answer").value = '';
+  document.getElementById('answer').value = '';
 }
 
-
 function press(num) {
-  document.getElementById("answer").value += num;
+  document.getElementById('answer').value += num;
 }
 
 function backspace() {
-  const input = document.getElementById("answer");
+  const input = document.getElementById('answer');
 
   input.value = input.value.slice(0, -1);
 }
 
 function submitAnswer() {
-  const userAns = Number(document.getElementById("answer").value);
+  const userAns = Number(document.getElementById('answer').value);
   const corrAns = Number(state.currProb.answer);
 
   if (state.checkAns(userAns, corrAns)) {
     state.score++;
     if (state.countProb === state.total) {
-      document.getElementById("score").value
-       = `${state.score} / ${state.total} = ${state.score / state.total * 100}%`;
-      movePage("result");
+      document.getElementById('score').value =
+        `${state.score} / ${state.total} = ${(state.score / state.total) * 100}%`;
+      movePage('result');
     } else {
       nextProblem();
     }
   } else {
     if (state.countWrong === 2) {
       state.countWrong = 0;
-      document.getElementById("answer").value = state.currProb.answer;
+      document.getElementById('answer').value = state.currProb.answer;
       setKeypadEnabled(false);
-      document.getElementById("submitAns").style.display = "none";
-      document.getElementById("wrongNext").style.display = "inline-block";
+      document.getElementById('submitAns').style.display = 'none';
+      document.getElementById('wrongNext').style.display = 'inline-block';
       setTimeout(() => {
-        document.getElementById("wrongNext").disabled = false;
+        document.getElementById('wrongNext').disabled = false;
       }, 1700);
     } else {
-      console.log("wrong");
+      console.log('wrong');
       state.countWrong++;
     }
-    
   }
 }
 
 function wrongNext() {
   if (state.countProb === state.total) {
-    document.getElementById("score").value
-      = `${state.score} / ${state.total} = ${state.score / state.total * 100}%`;
-    movePage("result");
+    document.getElementById('score').value =
+      `${state.score} / ${state.total} = ${(state.score / state.total) * 100}%`;
+    movePage('result');
   } else {
     nextProblem();
   }
-  document.getElementById("wrongNext").style.display = "none";
-  document.getElementById("submitAns").style.display = "inline-block";
+  document.getElementById('wrongNext').style.display = 'none';
+  document.getElementById('submitAns').style.display = 'inline-block';
   setKeypadEnabled(true);
 }
 
-
-document.getElementById("goMenu").addEventListener("click", () => {
+document.getElementById('goMenu').addEventListener('click', () => {
   Object.assign(state, INITIAL_STATE);
-  movePage("menu")
-})
+  movePage('menu');
+});
 
-document.getElementById("reQuiz").addEventListener("click", () => {
+document.getElementById('reQuiz').addEventListener('click', () => {
   state.score = 0;
   state.countProb = 0;
   nextProblem();
-  movePage("quiz");
-})
+  movePage('quiz');
+});
 
-
-
-const $keypadButtons =
-  document.querySelectorAll("#keypad button");
+const $keypadButtons = document.querySelectorAll('#keypad button');
 
 function setKeypadEnabled(enabled) {
-  $keypadButtons.forEach(button => {
+  $keypadButtons.forEach((button) => {
     button.disabled = !enabled;
   });
 }
--- data.js
+++ data.js
@@ -1,92 +1,83 @@
 export const probBook = {
   probs: {
-    add: { func: addGen, text: "덧셈" },
-    sub: { func: subGen, text: "뺄셈" },
-    mul: { func: mulGen, text: "곱셈" },
-    div: { func: divGen, text: "나눗셈" }
+    add: { func: addGen, text: '덧셈' },
+    sub: { func: subGen, text: '뺄셈' },
+    mul: { func: mulGen, text: '곱셈' },
+    div: { func: divGen, text: '나눗셈' },
   },
   Ids() {
-    return Object.entries(this.probs).map(([Id, val]) => (
-      { value: Id, text: val.text }
-    ));
+    return Object.entries(this.probs).map(([Id, val]) => ({
+      value: Id,
+      text: val.text,
+    }));
   },
   select(Id) {
     return this.probs[Id].func;
-  }
-
+  },
 };
 
 function addGen() {
   const [a, b] = [randomInt(1, 10), randomInt(1, 10)];
   return {
     probDisp: `${a} + ${b}`,
-    answer: (a+b),
-  }
+    answer: a + b,
+  };
 }
 
 function subGen() {
   const [a, b] = [randomInt(1, 10), randomInt(1, 10)];
   return {
     probDisp: `${a} - ${b}`,
-    answer: (a-b),
-  }
+    answer: a - b,
+  };
 }
 
 function mulGen() {
   const [a, b] = [randomInt(1, 10), randomInt(1, 10)];
   return {
     probDisp: `${a} × ${b}`,
-    answer: (a*b),
-  }
+    answer: a * b,
+  };
 }
 
 function divGen() {
   const [a, b] = [randomInt(1, 10), randomInt(1, 10)];
   return {
     probDisp: `${a} / ${b}`,
-    answer: (a/b),
-  }
+    answer: a / b,
+  };
 }
 
-const exactCheck = detail => (test, ref) => test === ref;
+const exactCheck = (detail) => (test, ref) => test === ref;
 
-export const percentCheck = detail => (test, ref) => {
+export const percentCheck = (detail) => (test, ref) => {
   if (ref === 0) {
-    return toleranceCheck(0.1)(test,ref);
+    return toleranceCheck(0.1)(test, ref);
   } else {
-    return Math.abs(test-ref)/Math.abs(ref) <= detail/100;
+    return Math.abs(test - ref) / Math.abs(ref) <= detail / 100;
   }
 };
 
-const toleranceCheck = detail => (test, ref) => (Math.abs(test-ref) <= detail);
-
+const toleranceCheck = (detail) => (test, ref) =>
+  Math.abs(test - ref) <= detail;
 
 export const checkAnsBook = {
   checkAnss: {
-    exact: { func: exactCheck, text: "정확히 일치"},
-    percent: { func: percentCheck, text: "오차율%" },
-    fix: { func: toleranceCheck, text: "허용오차" },
+    exact: { func: exactCheck, text: '정확히 일치' },
+    percent: { func: percentCheck, text: '오차율%' },
+    fix: { func: toleranceCheck, text: '허용오차' },
   },
   Ids() {
-    return Object.entries(this.checkAnss).map(([Id, val]) => (
-      { value: Id, text: val.text }
-    ));
+    return Object.entries(this.checkAnss).map(([Id, val]) => ({
+      value: Id,
+      text: val.text,
+    }));
   },
   select(Id, detail) {
     return this.checkAnss[Id].func(detail);
-  }
-
+  },
 };
 
-
-
-
-
-
-
-
 function randomInt(min, max) {
-  return Math.floor(
-    Math.random() * (max - min + 1)
-  ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
